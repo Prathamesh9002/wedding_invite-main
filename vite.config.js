9cd094696c62898539542a503e4897610c6b4bd5
ROCKET1\prajput <prajput@rocketsoftware.com>
2026-06-14 13:12:13 +0530

    Adding node modules

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   root: '.',
   publicDir: 'public',
-  base: '/wedding_invite/',
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
